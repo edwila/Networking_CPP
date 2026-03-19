acdf955b7a662f956f1810b346f481c26ef30f79
edwila <edwila@umich.edu>
2026-03-19 11:53:19 +0200

    Added primitive server commands

--- server/main.cpp
+++ server/main.cpp
@@ -8,29 +8,29 @@ int main(){
 
     syncer.init();
 
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-
-    std::cout << "Updating world now.\n";
-
-    entt::registry& world = syncer.get_world();
-
-    auto other_entity = syncer.create_entity();
-
-    auto entity = syncer.create_entity();
-
-    syncer.add_tag<Networked>(entity);
-
-    syncer.emplace<Postation, uint32_t>(entity, 0xFFFFFFFF);
-
-    syncer.emplace<Name, std::string>(entity, "Ismail");
-
-    syncer.get_world().emplace_or_replace<Name>(entity, "genscript");
-
     std::string user_entry;
 
     while(user_entry != "1"){
         std::cout << "Option: ";
         std::cin >> user_entry;
+
+        if(user_entry == "modify"){
+            std::cout << "Updating world now.\n";
+
+            entt::registry& world = syncer.get_world();
+
+            auto other_entity = syncer.create_entity();
+
+            auto entity = syncer.create_entity();
+
+            syncer.add_tag<Networked>(entity);
+
+            syncer.emplace<Postation, uint32_t>(entity, 0xFFFFFFFF);
+
+            syncer.emplace<Name, std::string>(entity, "Ismail");
+
+            syncer.get_world().emplace_or_replace<Name>(entity, "genscript");
+        }
     }
     
     return 0;
