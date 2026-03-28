9063fa5344194689883355f8c2d01cea0db6d035
edwila <edwila@umich.edu>
2026-03-28 18:06:07 +0200

    Fixed output when printing playerlist

--- server/main.cpp
+++ server/main.cpp
@@ -58,12 +58,12 @@ int main(){
 
             for(auto const& entry : players){
                 char str_addr[INET6_ADDRSTRLEN];
-                client* c = (client*)entry.second;
+                client* c = entry.second;
 
-                if(inet_ntop(AF_INET6, &(c->host), str_addr, INET6_ADDRSTRLEN) != nullptr){
+                if(inet_ntop(AF_INET, &(c->host), str_addr, INET_ADDRSTRLEN) != nullptr){
                     std::cout << "[" << entry.first << "]: " << str_addr << "\n";
                 } else{
-                    std::cout << "[" << entry.first << "]: " << entry.second << "\n";
+                    std::cout << "[" << entry.first << "]: " << c << "\n";
                 }
             }
         }
